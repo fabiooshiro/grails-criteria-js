3fb3d236ac8ef07cebf459d90a3d513b78cf6e50
fabio.oshiro <fabio.oshiro@gmail.com>
2013-05-04 10:14:56 -0300

    Apache License and short description

--- CriteriaJsGrailsPlugin.groovy
+++ CriteriaJsGrailsPlugin.groovy
@@ -9,11 +9,11 @@ class CriteriaJsGrailsPlugin {
     def title = "Criteria Js Plugin"
     def author = "Fabio Issamu Oshiro"
     def authorEmail = ""
-    def description = 'Brief summary/description of the plugin.'
+    def description = 'GORM criteria for javascript'
 
     def documentation = "http://grails.org/plugin/criteria-js"
 
-//    def license = "APACHE"
+    def license = "APACHE"
     def organization = [name: "Investtools", url: "http://www.investtools.com.br/"]
     def issueManagement = [system: 'GitHub', url: 'https://github.com/fabiooshiro/grails-criteria-js/issues']
     def scm = [url: 'https://github.com/fabiooshiro/grails-criteria-js']
